ec083cb2721653e267acaddf5e47e9447a08deb7
SamitSaha <samitsaha99@gmail.com>
2023-04-26 23:17:51 +0600

    travel guidence website
    this is my cse471-software engineering CSE course project of undergraduate degree.

---
+++ book.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <!-- font awesome cdn link -->
+    <link rel = "stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- custom css file  link -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="script.js">
+
+</head>
+<body>
+<!-- header section start -->
+
+<section class="header">
+
+    <a href="home.php" class="logo">TravelGuidence.</a>
+    <nav class="navbar">
+        <a href="home.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="package.php">Package</a>
+        <a href="book.php">Book</a>
+    </nav>
+    <div id="menu-btn" class="fas fa-bars"></div>
+</section>    
+<!-- header section end -->
+
+<div class="heading" style = "background: url(book-background.jpg) no-repeat">
+    <h1>Book Us</h1>
+</div>
+
+<!-- booking section starts -->
+
+<section class="booking">
+    <h1 class="heading-title"> Book your trip!</h1>
+
+    <form action="book_form.php" method="post" class="book-form">
+
+        <div class="flex">
+            <div class="inputBox">
+                <span>name :</span>
+                <input type="text" placeholder="enter your name " name="name">
+            </div>
+            <div class="inputBox">
+                <span>email :</span>
+                <input type="email" placeholder="enter you email" name="email">
+            </div>
+            <div class="inputBox">
+                <span>phone :</span>
+                <input type="number" placeholder="enter you phone number " name="phone">
+            </div>
+            <div class="inputBox">
+                <span>addres :</span>
+                <input type="text" placeholder="enter your address" name="address">
+            </div>
+            <div class="inputBox">
+                <span>where to :</span>
+                <input type="text" placeholder="place you want to visit" name="location">
+            </div>
+            <div class="inputBox">
+                <span>how many :</span>
+                <input type="number" placeholder="number of guests" name="guests">
+            </div>
+            <div class="inputBox">
+                <span>arrivals time:</span>
+                <input type="date" placeholder="date" name="arraivals">
+            </div>
+            <div class="inputBox">
+                <span>leaving time:</span>
+                <input type="date" placeholder="date" name="leaving">
+            </div>
+            
+
+        </div>
+
+        <input type="submit" value="submit" class="btn" name="send">
+    </form>
+    
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- booking section ends -->
+
+
+<!-- footer section starts -->
+<section class = "footer">
+    <div  class="box-container">
+
+        <div class="box">
+            <h3>Quick Links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+            <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+            <a href="package.php"> <i class="fas fa-angle-right"></i> Package</a>
+            <a href="book.php"> <i class="fas fa-angle-right"></i> Book</a>
+        </div>
+
+        <div class="box">
+            <h3>Extra Links</h3>
+            <a href="#"> <i class="fas fa-angle-right"></i> Ask Questions</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> About Us</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> Privacy Policy</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> Terms of use</a>
+        </div>
+
+        <div class="box">
+            <h3>Contact Info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +01681492198</a>
+            <a href="#"> <i class="fas fa-phone"></i> +01681492198</a>
+            <a href="#"> <i class="fas fa-envelop"></i> samitsah99@gmail.com</a>
+            <a href="#"> <i class="fas fa-map"></i> Dhaka,Bangladesh</a>
+        </div>
+
+        <div class="box">
+            <h3>Follow Us</h3>
+            <a href="#"> <i class="fas fa-facebook-f"></i> Facebook</a>
+            <a href="#"> <i class="fas fa-twitter"></i> Twitter</a>
+            <a href="#"> <i class="fas fa-instagram"></i> Instagram</a>
+            <a href="#"> <i class="fas fa-linkedin"></i> Linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit"> Created by <span>Mr. Samit Saha</span> | all rights reserved! </div>
+</section>
+<!-- footer section ends -->
+
+
+<!-- swiper js link -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+<!-- custom js life link -->
+    <script src="script.js"></script>
+</body>
+</html>
